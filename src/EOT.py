2350f0849c52cff6b2abd1766f9f43ab522c47ce
eons <eons@eons.dev>
2023-06-18 17:23:31 -0700

    increase precision

--- src/EOT.py
+++ src/EOT.py
@@ -9,7 +9,7 @@ class EOT():
 		day_of_year = now.timetuple().tm_yday
 		hour = now.hour
 		minute = now.minute
-		second = now.second
+		second = now.microsecond / 1000000
 
 		one_hour = 1 / 24 / 365
 		one_minute = one_hour / 60
@@ -17,13 +17,13 @@ class EOT():
 
 		exact_day = (day_of_year / 365)
 		exact_hour = hour * one_hour
-		exact_minute = now.minute * one_minute
-		exact_second = now.second * one_second
+		exact_minute = minute * one_minute
+		exact_second = second * one_second
 		exact_decimal = exact_day + exact_hour + exact_minute + exact_second
-		decimal = float('%.8f' % (exact_decimal))
+		decimal = float('%.12f' % (exact_decimal))
 		# error = decimal * 365 - day_of_year - (hour / 24)
 		# error_hours = error * 24
-		stardate = format(year + decimal, '.8f')
+		stardate = format(year + decimal, '.10f')
 
 		# print("stardate for", year, day_of_year, hour)
 		# print("error:", error, "in hours:", error_hours)
